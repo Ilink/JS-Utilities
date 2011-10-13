5985ae4545aceb6d9af27b34e85e3877f82084e3
Ian Link <ian@linkportfolio.net>
2011-10-12 19:07:21 -0700

    refactored examples

---
+++ js/samples.js
@@ -0,0 +1,24 @@
+var test = function(){
+    $('body').append('hi!')
+}
+
+var json = [{
+           'selector' : '#test',
+           'bind_to' : 'click',
+           'func' : test
+           },
+           {
+           'selector' : '#test2',
+               'bind_to' : 'click',
+               'func' : function(){
+                   $('body').append('hello!')
+               }
+           }
+];
+
+$(document).ready(function()
+{
+    $('#open_box').click(function(){
+        smallbox.init($('#content'));
+    });
+});
